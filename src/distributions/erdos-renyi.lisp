ae9e1c13f684032e82e32d3f1cd6209643165a4d
Akshay Srinivasan <akshaysrinivasan@gmail.com>
2018-02-22 00:52:06 +0900

    erdos-renyi.lisp: change sampler names to something more informative

--- src/distributions/erdos-renyi.lisp
+++ src/distributions/erdos-renyi.lisp
@@ -1,7 +1,7 @@
 (in-package #:matlisp)
 
-(defun gnp (n p)
-  "(GNP n p)
+(defun random-gnp (n p)
+  "(RANDOM-GNP n p)
   Samples from a Erdos-Renyi distribution of @arg{n} vertex graphs, whose edges are sampled with probability @arg{p}.
 
   [1] Batagelj, V., & Brandes, U. (2005). Efficient generation of large random networks. Physical Review E, 71(3), 036113."
@@ -18,8 +18,8 @@
 		  (ref ret w v) 1))))
     (copy ret (tensor 'index-type 'simple-graph-tensor))))
 
-(defun gnm (n m &aux (max-m (* n (- n 1) 1/2)))
-  "(GNM n m)
+(defun random-gnm (n m &aux (max-m (* n (- n 1) 1/2)))
+  "(RANDOM-GNM n m)
   Samples uniformly from the set of @arg{n} vertex graphs with @arg{m} edges.
 
   [1] Batagelj, V., & Brandes, U. (2005). Efficient generation of large random networks. Physical Review E, 71(3), 036113."
