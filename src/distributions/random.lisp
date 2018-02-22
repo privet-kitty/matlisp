07553b5bbc8949fa546bd018a4c700089112f02e
Akshay Srinivasan <akshaysrinivasan@gmail.com>
2018-02-22 21:20:17 +0900

    random.lisp: move bin sampler to random-multinomial

--- src/distributions/random.lisp
+++ src/distributions/random.lisp
@@ -1,11 +1,33 @@
+;; Copyright (c) 2018 Akshay Srinivasan
+
+;; This library is free software; you can redistribute it and/or modify it under
+;; the terms of the GNU Lesser General Public License as published by the
+;; Free Software Foundation; either version 2.1 of the License, or (at your option)
+;; any later version. Those exceptions, and interpretations specific to Lisp software,
+;; as published by Franz Inc., shall take precedence over LGPL.
+
+;; This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+;; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+;; See the GNU Lesser General Public License for more details.
+
+;; You should have received a copy of the GNU Lesser General Public License along with this
+;; library; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+;; Boston, MA 02111-1307 USA. Clarifications on the applicability of LGPL to Lisp software
+;; can be obtained from Franz Incorporated, Berkeley, CA 94704, USA.
+
 (in-package #:matlisp)
 
-(definline draw-standard-exponential ()
-  "Return a random variable from the Exponential(1) distribution, which has density exp(-x)."
-  ;; Adapted from cl-random, originally written by Tamas Papp
-  (- (log (- 1d0 (random 1d0)))))
+(defun random-multinomial (probabilities &optional (n #.(expt 2 32)))
+  (letv* ((cvec (iter (for pr-i in probabilities) (summing (floor (* pr-i n)) into zz)
+		      (collect zz into ret)
+		      (finally (return (coerce (cons 0 ret) 'simple-vector))))
+		:type simple-vector)
+	  (jj lb (binary-search (random (aref cvec (1- (length cvec)))) 0 (length cvec)
+				(the simple-vector cvec))))
+    (or jj (1- lb))))
 
 ;;
+#+nil
 (defmacro generate-rand (func type clause)
   (let ((clause (etypecase clause (symbol `(,clause)) (cons clause)))
 	(func! (intern (string+ (symbol-name func) "!"))))
@@ -39,11 +61,4 @@
 	ret)
       (random arg)))
 
-#+nil
-(defun bin-sampler (ps &optional (n most-positive-fixnum) (state *mt-random-state*))
-  (letv* ((cvec (iter (for pp in ps) (summing (floor (* pp n)) into zz)
-		      (collect zz into ret)
-		      (finally (return (coerce (cons 0 ret) 'simple-vector))))
-		:type simple-vector)
-	  (jj lb (binary-search (mt-random (aref cvec (1- (length cvec))) state) 0 (length cvec) (the simple-vector cvec))))
-    (or jj (1- lb))))
+
