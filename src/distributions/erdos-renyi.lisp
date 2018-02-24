79809e21190de330bde739257f235fba201e4978
Akshay Srinivasan <akshaysrinivasan@gmail.com>
2018-02-24 17:34:39 +0900

    update LICENSE

--- src/distributions/erdos-renyi.lisp
+++ src/distributions/erdos-renyi.lisp
@@ -1,3 +1,20 @@
+;; Copyright (c) 2018 Akshay Srinivasan <akshaysrinivasan@gmail.com>
+
+;; This library is free software; you can redistribute it and/or modify it under
+;; the terms of the GNU Lesser General Public License as published by the
+;; Free Software Foundation; either version 2.1 of the License, or (at your option)
+;; any later version. Those exceptions, and interpretations specific to Lisp software,
+;; as published by Franz Inc., shall take precedence over LGPL.
+
+;; This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+;; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+;; See the GNU Lesser General Public License for more details.
+
+;; You should have received a copy of the GNU Lesser General Public License along with this
+;; library; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+;; Boston, MA 02111-1307 USA. Clarifications on the applicability of LGPL to Lisp software
+;; can be obtained from Franz Incorporated, Berkeley, CA 94704, USA.
+
 (in-package #:matlisp)
 
 (defun random-gnp (n p)
