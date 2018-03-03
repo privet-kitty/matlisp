aecc8b92cd5fbd5ca587e0e12060d346a9b15f38
Ben Dudson <bd512@york.ac.uk>
2018-03-03 23:07:36 +0000

    Add as-array 1D vector case
    If the tensor is 1D, just return the store
    rather than creating a displaced array.

--- src/base/interop.lisp
+++ src/base/interop.lisp
@@ -3,9 +3,8 @@
 
 (in-package #:matlisp)
 
-
 (defgeneric as-tensor (object)
-  (:documentation "Returns OBJECT as a Matlisp tensor type. May share storage."))
+  (:documentation "Returns OBJECT as a Matlisp tensor type. May share storage or copy."))
 
 (defmethod as-tensor ((object tensor))
   "If already a tensor then just return"
@@ -26,7 +25,7 @@
 ;;;
 
 (defgeneric as-array (object)
-  (:documentation "Returns OBJECT as a CL array type. May share storage. "))
+  (:documentation "Returns OBJECT as a CL array type. May share storage or copy. "))
 
 (defmethod as-array ((object array))
   "If already an array, just return"
@@ -35,6 +34,10 @@
 (defmethod as-array ((object tensor))
   "Make an array of the same dimensions as OBJECT, displaced to the storage vector.
    Resulting array shares storage with OBJECT. "
+  ;; If 1D tensor, return the store directly
+  (when (= 1 (length (dimensions object)))
+    (slot-value object 'store))
+  ;; If > 1D, create displaced array
   (with-slots (store dimensions) object
     (make-array (map 'list #'identity dimensions) ; Convert dimensions to list
                 :element-type (array-element-type store)
